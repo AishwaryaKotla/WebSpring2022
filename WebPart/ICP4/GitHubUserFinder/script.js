44d4c4c7618e307264e8895af05819e7ce6844cb
sunnypranay4748 <98367465+sunnypranay4748@users.noreply.github.com>
2022-02-08 14:03:59 -0600

    Added html code , made changes to script.js and added a styles file

--- WebPart/ICP4/GitHubUserFinder/script.js
+++ WebPart/ICP4/GitHubUserFinder/script.js
@@ -1,19 +1,41 @@
 function getGithubInfo(user) {
     //1. Create an instance of XMLHttpRequest class and send a GET request using it.
     // The function should finally return the object(it now contains the response!)
+    $.ajax({
+        "url": "https://api.github.com/users/" + user,
+        "type": 'GET',
+        "success": function (res) {
+            showUser(res);
+        },
+        "fail": noSuchUser(user),
+    });
 }
 
 function showUser(user) {
     //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+    $(".avatar").show();
+    $(".information").show();
+    $("#profile h1").text(user.login);
+    $(".avatar img").attr("src", user.avatar_url);
+    $("#name").text(user.name == null ? "null" : user.name);
+    $("#loginId").text(user.id);
+    $("#git").attr("href",user.html_url);
+    $("#followers").text(user.followers);
+    $("#creationDate").text(user.created_at);
+    console.log($("#git").attr("href"));
 }
 
 function noSuchUser(username) {
     //3. set the elements such that a suitable message is displayed
+    $("#profile h1").text(username + " NOT FOUND");
+    $(".avatar").hide();
+    $(".information").hide();
 }
 
 $(document).ready(function () {
     $(document).on('keypress', '#username', function (e) {
         //check if the enter(i.e return) key is pressed
+        let username;
         if (e.which == 13) {
             //get what the user enters
             username = $(this).val();
@@ -25,9 +47,10 @@ $(document).ready(function () {
             if (response.status == 200) {
                 showUser(JSON.parse(response.responseText));
                 //else display suitable message
-            } else {
-                noSuchUser(username);
-            }
+            } 
+            //else {
+              //  noSuchUser(username);
+           // }
         }
     })
 });
