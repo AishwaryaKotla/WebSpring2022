c0814de433d4c1df86a9bfca5fa070b98f1aca01
sunnypranay4748 <47515244+AishwaryaKotla@users.noreply.github.com>
2022-02-08 19:40:44 -0600

    added folder

---
+++ WebPart/ICP4/GitHubUserFinder/script.js
@@ -0,0 +1,33 @@
+function getGithubInfo(user) {
+    //1. Create an instance of XMLHttpRequest class and send a GET request using it.
+    // The function should finally return the object(it now contains the response!)
+}
+
+function showUser(user) {
+    //2. set the contents of the h2 and the two div elements in the div '#profile' with the user content
+}
+
+function noSuchUser(username) {
+    //3. set the elements such that a suitable message is displayed
+}
+
+$(document).ready(function () {
+    $(document).on('keypress', '#username', function (e) {
+        //check if the enter(i.e return) key is pressed
+        if (e.which == 13) {
+            //get what the user enters
+            username = $(this).val();
+            //reset the text typed in the input
+            $(this).val("");
+            //get the user's information and store the respsonse
+            response = getGithubInfo(username);
+            //if the response is successful show the user's details
+            if (response.status == 200) {
+                showUser(JSON.parse(response.responseText));
+                //else display suitable message
+            } else {
+                noSuchUser(username);
+            }
+        }
+    })
+});
