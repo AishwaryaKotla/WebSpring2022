8dca696bca5b97fd32609b2efc626dc16a1f01d3
sunnypranay4748 <47515244+AishwaryaKotla@users.noreply.github.com>
2022-02-18 17:04:06 -0600

    added src

---
+++ WebPart/ICP5/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // define list of items
+  items= [];
+
+  // Write code to push new item
+  submitNewItem() {
+
+  }
+
+  // Write code to complete item
+  completeItem() {
+
+  }
+
+  // Write code to delete item
+  deleteItem() {
+
+  }
+
+}
