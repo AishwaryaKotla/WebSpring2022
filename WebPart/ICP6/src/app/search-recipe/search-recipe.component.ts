1077377f07f2b42e597dac536dc64d9d4eb64dd3
AishwaryaKotla <47515244+AishwaryaKotla@users.noreply.github.com>
2022-02-22 13:45:14 -0600

    created mashup given in the assignment

---
+++ WebPart/ICP6/src/app/search-recipe/search-recipe.component.ts
@@ -0,0 +1,51 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-search-recipe',
+  templateUrl: './search-recipe.component.html',
+  styleUrls: ['./search-recipe.component.css']
+})
+export class SearchRecipeComponent implements OnInit {
+  @ViewChild('recipe') recipes: ElementRef;
+  @ViewChild('place') places: ElementRef;
+  recipeValue: any;
+  placeValue: any;
+  venueList = [];
+  recipeList = [];
+
+  currentLat: any;
+  currentLong: any;
+  geolocationPosition: any;
+
+  constructor(private _http: HttpClient) {
+  }
+
+  ngOnInit() {
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.geolocationPosition = position;
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;
+      });
+  }
+
+  getVenues() {
+
+    this.recipeValue = this.recipes.nativeElement.value;
+    this.placeValue = this.places.nativeElement.value;
+
+    if (this.recipeValue !== null) {
+      /**
+       * Write code to get recipe
+       */
+    }
+
+    if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null && this.recipeValue !== '') {
+      /**
+       * Write code to get place
+       */
+    }
+  }
+}
