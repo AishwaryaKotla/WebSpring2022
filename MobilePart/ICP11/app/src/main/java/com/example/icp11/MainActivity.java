4cb41fe671f7b5624ee4142116656de7eb2188d9
Pranay Reddy <98367465+sunnypranay4748@users.noreply.github.com>
2022-04-22 22:52:32 -0500

    uploading code

---
+++ MobilePart/ICP11/app/src/main/java/com/example/icp11/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.icp11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edittext);
+        button = findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if(status==TextToSpeech.SUCCESS){
+                            int result =textToSpeech.setLanguage(Locale.ENGLISH);
+                        if(result==TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA){
+                            Log.e("message","language not supported");
+                        }
+                        else{
+                            textToSpeech.speak(String.valueOf(editText.getText()),TextToSpeech.QUEUE_ADD, null);
+                        }
+
+                        }
+                        else{
+                            Log.e("message","TT's not supporting");
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
